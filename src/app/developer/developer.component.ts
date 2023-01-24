508ab4e5e16be1dff8f6883783a7e725addfa9d4
FIRST\_NAME LAST\_NAME <Mack-Jack-2-Dijon\jcqs.thm@gmail.com>
2023-01-24 12:14:48 +0100

    4

--- src/app/developer/developer.component.ts
+++ src/app/developer/developer.component.ts
@@ -16,26 +16,26 @@ export class DeveloperComponent {
     [
       {
         name: 'Billy Gibbons',
-        logo: 'https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw',
+        logo: 'https://media.tenor.com/c2cCt7lJS-QAAAAC/zztop-guitarspin.gif',
         site: 'https://www.youtube.com/watch?v=Ae829mFAGGE'
       },
       {
         name: 'Frank Beard',
-        logo: 'https://fr.wikipedia.org/wiki/ZZ_Top#/media/Fichier:ZZ_Top_Live.jpg',
+        logo: 'https://media.tenor.com/YQjMEbUxm7gAAAAd/zz-top-legs.gif',
         site: 'https://www.youtube.com/watch?v=Gg9cNGHl-bg'
       },
       {
         name: 'Lanier Greig',
-        logo: 'https://fr.wikipedia.org/wiki/ZZ_Top#/media/Fichier:Zztop.jpg',
+        logo: 'https://media.tenor.com/QZRB6gUWu8sAAAAd/brushing-beard-beard.gif',
         site: 'https://www.youtube.com/watch?v=Z_4ULKpkLNc'
       },
       { name: 'Dan Mitchell',
-        logo: 'https://fr.wikipedia.org/wiki/ZZ_Top#/media/Fichier:Zz_top_performance.jpg',
+        logo: 'https://media.tenor.com/9wLpYmMhneoAAAAC/car-speed.gif',
         site: 'https://www.youtube.com/watch?v=kaIZWjItReI'
       },
       {
         name: 'Billy Ethridge',
-        logo: 'https://fr.wikipedia.org/wiki/ZZ_Top#/media/Fichier:ZZTopEliminator.jpg',
+        logo: 'https://media.tenor.com/0pFJjj9_6RwAAAAC/zz-top-legs.gif',
         site: 'https://www.youtube.com/watch?v=yqzUsATxom4'
       },
     ]
